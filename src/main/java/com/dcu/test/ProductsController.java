ebc8b73ef8867aecc2da16f993b131e8e53184a7
kyuchan <xla5193@navet.com>
2025-03-24 16:04:00 +0900

    commit

--- src/main/java/com/dcu/test/ProductsController.java
+++ src/main/java/com/dcu/test/ProductsController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -18,10 +20,24 @@ public class ProductsController {
         model.addAttribute("products", products);
         return "productList";
     }
+
     @GetMapping("/productRegister")
     String productRegister(Model model) {
-        List<Product> products = productRepository.findAll();
-        model.addAttribute("products", products);
-        return "productRegister";
+        return "productRegistration";
+    }
+
+    @PostMapping("/productRegister")
+    String productRegister(@ModelAttribute Product product) {
+        System.out.println(product.getImage());
+        System.out.println(product.getTitle());
+        System.out.println(product.getCompany());
+        System.out.println(product.getPrice());
+        System.out.println(product.getRelease_date());
+
+        // JPA를 통해 데이터베이스에 저장
+        productRepository.save(product);
+
+        // 저장 후 상품 목록 페이지로 이동
+        return "redirect:/productList";
     }
 }
